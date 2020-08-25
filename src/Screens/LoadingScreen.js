3c7ced1010bc1239effa63f049590dec6dffd169
Govinda <govindavashishtha@gmail.com>
2020-08-26 00:03:59 +0530

    added carousel to the loginScreen

--- src/Screens/LoadingScreen.js
+++ src/Screens/LoadingScreen.js
@@ -1,20 +1,71 @@
 import React from 'react';
 import {
-  View,Text,ImageBackground,StyleSheet
+  View,Text,ImageBackground,StyleSheet,Dimensions
 } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import ThemeButton from '../Components/ThemeButton';
 import ScreenNames from './../Constants/ScreenNames'
-const image = { uri: "https://picsum.photos/food/picsum/900/300" };
-
-
+const screen = Dimensions.get("screen");
+const renderItem = ({item , index}) =>{
+  return (
+    <View style={{
+        backgroundColor:'floralwhite',
+        borderRadius: 5,
+        height: 400,
+        padding: 50,
+        marginLeft: 25,
+        marginRight: 25, }}>
+      {/* todo */}
+      <Text style={{fontSize: 30}}>{item.title}</Text>
+      <Text>{item.text}</Text>
+    </View>
+  )
+}
+const  carouselItems =  [
+  {
+      title:"Item 1",
+      text: "Text 1",
+  },
+  {
+      title:"Item 2",
+      text: "Text 2",
+  },
+  {
+      title:"Item 3",
+      text: "Text 3",
+  },
+  {
+      title:"Item 4",
+      text: "Text 4",
+  },
+  {
+      title:"Item 5",
+      text: "Text 5",
+  },
+]
 const LoadingScreen = ({navigation})=>{
+    const loginHandler = ()=>{
+      
+    }
     return (
       <View style={styles.container}>
     <ImageBackground source={require('./../assets/loading.jpg')} style={styles.image}>
-        <View style={styles.container}> 
+        <View style={styles.ImageContainer}> 
         <Text style={styles.text}>Inside</Text>
+        <View style ={ { flex: 1, flexDirection:'row', justifyContent: 'center', }}>
+        <Carousel
+              layout={'stack'}
+              data={carouselItems}
+              renderItem={renderItem}
+              sliderWidth={screen.width}
+              itemWidth={350}
+              autoplay={true}
+              autoplayInterval={1500}
+            />
+        </View>
+       
         <View>
-        <ThemeButton onPress={()=>{navigation.navigate(ScreenNames.LogInStack)}} title={'LogIn'}/>
+        <ThemeButton onPress={()=>{navigation.navigate(ScreenNames.LogInStack)}} title={'Log In'}/>
         </View>
         
         </View>
@@ -27,6 +78,13 @@ const LoadingScreen = ({navigation})=>{
     )
 }
 const styles = StyleSheet.create({
+  ImageContainer: {
+    paddingVertical:20,
+    paddingHorizontal:10,
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between"
+  },
   container: {
     flex: 1,
     flexDirection: "column"
