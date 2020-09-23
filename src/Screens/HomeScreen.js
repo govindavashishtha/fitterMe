c7d5e119ecb796afe6f1393619c740bb6c11a1ee
Govinda <govindavashishtha@gmail.com>
2020-09-23 21:37:24 +0530

    called news data and rendered it to flatList

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import {
   StyleSheet,
-  View,Text
+  View, Text, FlatList
 } from 'react-native';
 import Header from './../Components/Header';
 import ThemeButton from '../Components/ThemeButton';
+import { useSelector } from 'react-redux';
+import NewsCard from '../Components/NewsCard';
 
-const HomeScreen = ({navigation})=>{
-    return (
-      <>
-         <Header title ={'Home'}/>
-         <View style = {styles.container}></View>
-      </>  
-    )
+const HomeScreen = ({ navigation }) => {
+  const news = useSelector(state => state.data)
+  return (
+    <>
+      <Header title={'Home'} />
+      <View style={styles.container}>
+          <FlatList
+            data={news}
+            renderItem={({ item }) => (
+              <NewsCard
+                Item={item}
+              />
+            )}
+            scrollEnabled={true}
+            horizontal={false}
+            keyExtractor={(item, index) => index.toString()}
+            contentContainerStyle={{
+              flexGrow: 1,
+            }}
+          />
+
+      </View>
+    </>
+  )
 }
 const styles = StyleSheet.create({
-  container:{
-    height:'100%',
+  container: {
+    flex:1,
+    height: '100%',
+    paddingHorizontal: 10,
   },
 })
 
