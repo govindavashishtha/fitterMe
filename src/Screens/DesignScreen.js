e6a6513b5a95f14dd4a42db506de3653dd119604
Aman <amansharma10221@gmail.com>
2020-11-03 13:04:58 +0530

    recipe api added

--- src/Screens/DesignScreen.js
+++ src/Screens/DesignScreen.js
@@ -3,18 +3,34 @@ import {
   StyleSheet,
   View,Text,
 } from 'react-native';
+import { Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Colors from '../Constants/Colors';
 import Header from './../Components/Header';
 
 const DesignScreen = ()=>{
 
+  const fetchAPI = () => {
+    console.log('refreshed');
+    let YOUR_APP_ID = '0dd5e843';
+    let YOUR_APP_KEY = '3ffcdd627a25d5dbf157a3a3854af603';
+    fetch(`https://api.edamam.com/api/nutrition-data?app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&ingr=1%20large%20mango`, {
+      method: 'GET'
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
     return (
       <>
        <Header title={'Design'}/>
        <View style={styles.container}>
-        <Icon name={'paperclip'} size={30} color={Colors.charcoalGrey80} />
-        <Text style={styles.text}>Coming Soon</Text>
+        <Button onPress={fetchAPI}>get</Button>
        </View>
       </>  
     )
