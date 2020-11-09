8b30a6d506d852a3414103912db6d7bbf591af48
Govinda <govindavashishtha@gmail.com>
2020-11-09 12:10:14 +0530

    news api conflict resolved

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -12,10 +12,14 @@ import { ScrollView } from 'react-native-gesture-handler';
 const HomeScreen = ({ navigation }) => {
   const [news, setNews] = useState(useSelector(state => state.data));
   const [refreshing, setRefreshing] = useState(false);
+  const apiKeys = ['59ed4d1096c14181ac87f374a460e0c1',
+  '4885a26a44d14c6cb3bd5aed4a203884',
+  '5329bf3ba3b840f9b426171a1bf4221f',
+  '927de1a2949a49f9aa2c7e1b973c3df4'];  
 
   const fetchAPI = () => {
     console.log('refreshed');
-    fetch(`https://saurav.tech/NewsAPI/top-headlines/category/health/in.json`, {
+    fetch(`http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${apiKeys[Math.floor(Math.random() * 4)]}`, {
       method: 'GET'
     })
       .then((response) => response.json())
--- src/Screens/LoadingScreen.js
+++ src/Screens/LoadingScreen.js
@@ -1,79 +1,82 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
-  View,Text,StyleSheet
+  View, Text, StyleSheet
 } from 'react-native';
 import Colors from '../Constants/Colors';
 import ScreenNames from './../Constants/ScreenNames';
 import auth from '@react-native-firebase/auth';
 import LoadingDots from "react-native-loading-dots";
 import database from '@react-native-firebase/database';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setData, setUser } from '../react-redux/actions';
 
-const LoadingScreen = ({navigation})=>{
+const LoadingScreen = ({ navigation }) => {
+  const apiKeys = ['59ed4d1096c14181ac87f374a460e0c1',
+    '4885a26a44d14c6cb3bd5aed4a203884',
+    '5329bf3ba3b840f9b426171a1bf4221f',
+    '927de1a2949a49f9aa2c7e1b973c3df4'];  
+  const dispatch = useDispatch();
 
-   const dispatch = useDispatch();
-
-   const fetchAPI=()=>{
-    fetch('https://saurav.tech/NewsAPI/top-headlines/category/health/in.json', {
+  const fetchAPI = () => {
+    fetch(`http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=${apiKeys[Math.floor(Math.random() * 4)]}`, {
       method: 'GET'
-  })
-  .then((response) => response.json())
-  .then((responseJson) => {
-      dispatch(setData(responseJson.articles))
-  })
-  .catch((error) => {
-      console.error(error);
-  });
-  }
-  const checkUser = async()=>{
-    if(auth().currentUser){
-      const phone = await auth().currentUser.phoneNumber;
-     await database()
-      .ref().child('Users').child(phone)
-      .once('value')
-      .then(snapshot => {
-        if(snapshot.val() && snapshot.val().firstName){
-          console.log(snapshot.val());
-          dispatch(setUser(snapshot.val()));
-          navigation.navigate(ScreenNames.TabStack);
-        }else{
-          navigation.navigate(ScreenNames.LogInStack);                                                           
-        }
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        dispatch(setData(responseJson.articles))
+      })
+      .catch((error) => {
+        console.error(error);
       });
-    }else{
-     navigation.navigate(ScreenNames.LogInStack);
+  }
+  const checkUser = async () => {
+    if (auth().currentUser) {
+      const phone = await auth().currentUser.phoneNumber;
+      await database()
+        .ref().child('Users').child(phone)
+        .once('value')
+        .then(snapshot => {
+          if (snapshot.val() && snapshot.val().firstName) {
+            console.log(snapshot.val());
+            dispatch(setUser(snapshot.val()));
+            navigation.navigate(ScreenNames.TabStack);
+          } else {
+            navigation.navigate(ScreenNames.LogInStack);
+          }
+        });
+    } else {
+      navigation.navigate(ScreenNames.LogInStack);
     }
- }
+  }
   useEffect(() => {
     fetchAPI();
-    setTimeout(checkUser,2000);
+    setTimeout(checkUser, 2000);
   }, [navigation]);
-    return (
-      <View style={styles.container}>
-       <Text onPress={()=>{
-         navigation.navigate(ScreenNames.LogInStack);
-       }} style={styles.logoText}>fitterMe</Text>
-       <View style={{ width:'10%',paddingVertical:30,marginLeft:'45%'}}>
-        <LoadingDots dots={3} colors={['#FFFFFF','#FFFFFF','#FFFFFF']} size={5} />
-       </View>
-       
+  return (
+    <View style={styles.container}>
+      <Text onPress={() => {
+        navigation.navigate(ScreenNames.LogInStack);
+      }} style={styles.logoText}>fitterMe</Text>
+      <View style={{ width: '10%', paddingVertical: 30, marginLeft: '45%' }}>
+        <LoadingDots dots={3} colors={['#FFFFFF', '#FFFFFF', '#FFFFFF']} size={5} />
       </View>
-    )
+
+    </View>
+  )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    height:'100%',
-    backgroundColor:Colors.primaryColorDark,
-    justifyContent:'center'
+    height: '100%',
+    backgroundColor: Colors.primaryColorDark,
+    justifyContent: 'center'
   },
   logoText: {
     color: Colors.white,
     fontSize: 40,
-    fontFamily:'Pacifico-Regular',
-    textAlign:'center'
+    fontFamily: 'Pacifico-Regular',
+    textAlign: 'center'
   }
 });
 
