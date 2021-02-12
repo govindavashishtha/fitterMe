c7e3dd511bf26ba1a5327aff2d30af760206768d
Govinda Vashishtha <govindavashishtha@gmail.com>
2021-02-12 12:12:49 +0530

    design Section made dark

--- src/Components/CaloriesCard.js
+++ src/Components/CaloriesCard.js
@@ -1,107 +1,196 @@
 import React from 'react';
-import {
-    StyleSheet,
-    View, Text,
-} from 'react-native';
-
+import {StyleSheet, View, Text} from 'react-native';
+import {useSelector} from 'react-redux';
+import Colors from '../Constants/Colors';
 
 const CaloriesCard = ({Item}) => {
-    return(
-        <>
-            <View style ={styles.container}>
-                {Item.calories ? (
-                    <Text style={styles.text}>
-                    Total Calories : {Item.calories} Kcal
-                </Text>
-                ) : null}
+  const isDarkMode = useSelector((state) => state.isDarkMode);
 
-                {Item.totalWeight ? (
-                    <Text style={styles.text}>
-                    Total Weight : {Item.totalWeight.toFixed(2)} g
-                </Text>
-                ) : null}
-                
-                {/* <ExtenedLine /> */}
-                {Item.totalNutrients.PROCNT && Item.totalNutrients? (
-                    <Text style={styles.text}>
-                {Item.totalNutrients.PROCNT.label} : {Item.totalNutrients.PROCNT.quantity.toFixed(2)} {Item.totalNutrients.PROCNT.unit}
-                </Text>
-                ) : null}
-               
-                {/* <ExtenedLine /> */}
-                {Item.totalNutrients.CHOCDF ? (<Text style={styles.text}>
-                {Item.totalNutrients.CHOCDF.label} : {Item.totalNutrients.CHOCDF.quantity.toFixed(2)} {Item.totalNutrients.CHOCDF.unit}
-                </Text>) : (<></>)}
-                
-                {/* <ExtenedLine /> */}
-                {Item.totalNutrients.FAT ? (<Text style={styles.text}>
-                Total {Item.totalNutrients.FAT.label} : {Item.totalNutrients.FAT.quantity.toFixed(2)} {Item.totalNutrients.FAT.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.FASAT ? (<Text style={styles.text1}>
-                {Item.totalNutrients.FASAT.label} Fat : {Item.totalNutrients.FASAT.quantity.toFixed(2)} {Item.totalNutrients.FASAT.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.FAT && Item.totalNutrients.FASAT ? (<Text style={styles.text1}>
-                Unsaturated Fat : {(Item.totalNutrients.FAT.quantity - Item.totalNutrients.FASAT.quantity).toFixed(2)} {Item.totalNutrients.FAPU.unit}
-                </Text>) : (<></>)}
-                
-                {/* <ExtenedLine /> */}
-                <Text style={styles.text}>Macros and Micros</Text>
-                {Item.totalNutrients.FE ? (<Text style={styles.text1}>
-                {Item.totalNutrients.FE.label} : {Item.totalNutrients.FE.quantity.toFixed(2)} {Item.totalNutrients.FE.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.K ? (<Text style={styles.text1}>
-                {Item.totalNutrients.K.label} : {Item.totalNutrients.K.quantity.toFixed(2)} {Item.totalNutrients.K.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.MG ? (<Text style={styles.text1}>
-                {Item.totalNutrients.MG.label} : {Item.totalNutrients.MG.quantity.toFixed(2)} {Item.totalNutrients.MG.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.NA ? (<Text style={styles.text1}>
-                {Item.totalNutrients.NA.label} : {Item.totalNutrients.NA.quantity.toFixed(2)} {Item.totalNutrients.NA.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.ZN ? (<Text style={styles.text1}>
-                {Item.totalNutrients.ZN.label} : {Item.totalNutrients.ZN.quantity.toFixed(2)} {Item.totalNutrients.ZN.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.P ? (<Text style={styles.text1}>
-                {Item.totalNutrients.P.label} : {Item.totalNutrients.P.quantity.toFixed(2)} {Item.totalNutrients.P.unit}
-                </Text>) : (<></>)}
-                
-                {/* <ExtenedLine /> */}
-                <Text style={styles.text}>Vitamins</Text>
-                {Item.totalNutrients.VITA_RAE ? (<Text style={styles.text1}>
-                {Item.totalNutrients.VITA_RAE.label} : {Item.totalNutrients.VITA_RAE.quantity.toFixed(2)} {Item.totalNutrients.VITA_RAE.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.VITC ? (<Text style={styles.text1}>
-                {Item.totalNutrients.VITC.label} : {Item.totalNutrients.VITC.quantity.toFixed(2)} {Item.totalNutrients.VITC.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.VITD ? (<Text style={styles.text1}>
-                {Item.totalNutrients.VITD.label} : {Item.totalNutrients.VITD.quantity.toFixed(2)} {Item.totalNutrients.VITD.unit}
-                </Text>) : (<></>)}
-                {Item.totalNutrients.TOCPHA ? (<Text style={styles.text1}>
-                {Item.totalNutrients.TOCPHA.label} : {Item.totalNutrients.TOCPHA.quantity.toFixed(2)} {Item.totalNutrients.TOCPHA.unit}
-                </Text>) : (<></>)}
-                
-            </View>
-        </>
-    )
-}
-const styles =  StyleSheet.create({
-    container:{
-        borderWidth:1,
-        borderRadius: 10,
-        padding: 20,
-        width: '70%',
-        alignItems: 'baseline',
-        justifyContent: 'center'
+  const styles = StyleSheet.create({
+    container: {
+      borderWidth: 1,
+      borderRadius: 10,
+      borderColor: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80,
+      padding: 20,
+      width: '70%',
+      alignItems: 'baseline',
+      justifyContent: 'center',
     },
     text: {
-        fontFamily: 'Karla-Bold',
-        fontSize: 16,
-        marginTop: 10,
+      fontFamily: 'Karla-Bold',
+      fontSize: 16,
+      marginTop: 10,
+      color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80,
     },
     text1: {
-        fontFamily: 'Karla-Regular',
-        fontSize: 14,
-    }
-})
+      fontFamily: 'Karla-Regular',
+      fontSize: 14,
+      color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80,
+    },
+  });
+  return (
+    <>
+      <View style={styles.container}>
+        {Item.calories ? (
+          <Text style={styles.text}>Total Calories : {Item.calories} Kcal</Text>
+        ) : null}
 
-export default CaloriesCard
+        {Item.totalWeight ? (
+          <Text style={styles.text}>
+            Total Weight : {Item.totalWeight.toFixed(2)} g
+          </Text>
+        ) : null}
+
+        {/* <ExtenedLine /> */}
+        {Item.totalNutrients.PROCNT && Item.totalNutrients ? (
+          <Text style={styles.text}>
+            {Item.totalNutrients.PROCNT.label} :{' '}
+            {Item.totalNutrients.PROCNT.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.PROCNT.unit}
+          </Text>
+        ) : null}
+
+        {/* <ExtenedLine /> */}
+        {Item.totalNutrients.CHOCDF ? (
+          <Text style={styles.text}>
+            {Item.totalNutrients.CHOCDF.label} :{' '}
+            {Item.totalNutrients.CHOCDF.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.CHOCDF.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+
+        {/* <ExtenedLine /> */}
+        {Item.totalNutrients.FAT ? (
+          <Text style={styles.text}>
+            Total {Item.totalNutrients.FAT.label} :{' '}
+            {Item.totalNutrients.FAT.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.FAT.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.FASAT ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.FASAT.label} Fat :{' '}
+            {Item.totalNutrients.FASAT.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.FASAT.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.FAT && Item.totalNutrients.FASAT ? (
+          <Text style={styles.text1}>
+            Unsaturated Fat :{' '}
+            {(
+              Item.totalNutrients.FAT.quantity -
+              Item.totalNutrients.FASAT.quantity
+            ).toFixed(2)}{' '}
+            {Item.totalNutrients.FAPU.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+
+        {/* <ExtenedLine /> */}
+        <Text style={styles.text}>Macros and Micros</Text>
+        {Item.totalNutrients.FE ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.FE.label} :{' '}
+            {Item.totalNutrients.FE.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.FE.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.K ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.K.label} :{' '}
+            {Item.totalNutrients.K.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.K.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.MG ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.MG.label} :{' '}
+            {Item.totalNutrients.MG.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.MG.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.NA ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.NA.label} :{' '}
+            {Item.totalNutrients.NA.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.NA.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.ZN ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.ZN.label} :{' '}
+            {Item.totalNutrients.ZN.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.ZN.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.P ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.P.label} :{' '}
+            {Item.totalNutrients.P.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.P.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+
+        {/* <ExtenedLine /> */}
+        <Text style={styles.text}>Vitamins</Text>
+        {Item.totalNutrients.VITA_RAE ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.VITA_RAE.label} :{' '}
+            {Item.totalNutrients.VITA_RAE.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.VITA_RAE.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.VITC ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.VITC.label} :{' '}
+            {Item.totalNutrients.VITC.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.VITC.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.VITD ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.VITD.label} :{' '}
+            {Item.totalNutrients.VITD.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.VITD.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+        {Item.totalNutrients.TOCPHA ? (
+          <Text style={styles.text1}>
+            {Item.totalNutrients.TOCPHA.label} :{' '}
+            {Item.totalNutrients.TOCPHA.quantity.toFixed(2)}{' '}
+            {Item.totalNutrients.TOCPHA.unit}
+          </Text>
+        ) : (
+          <></>
+        )}
+      </View>
+    </>
+  );
+};
+
+export default CaloriesCard;
--- src/Components/MealCard.js
+++ src/Components/MealCard.js
@@ -1,63 +1,81 @@
 import React from 'react';
-import {
-    StyleSheet,
-    View, Text, TouchableOpacity
-} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import Colors from '../Constants/Colors';
 import database from '@react-native-firebase/database';
 import auth from '@react-native-firebase/auth';
 import ConfirmDialog from './ConfirmDialog';
+import {useSelector} from 'react-redux';
 
-const MealCard = ({ Item, title, mealTime , refresh }) => {
-    const phone = auth().currentUser.phoneNumber;
-    const deleteItem =  async() => {
-        await database()
-            .ref('/Users/').child(phone).child('diet').child(mealTime).child(title)
-            .set(null).then(()=>{
-                refresh();
-            });
-    }
-    return (
-        <TouchableOpacity onLongPress={()=>{ConfirmDialog('Delete', 'Sure to Delete', deleteItem)}}>
-        <View style={styles.container}>
-            <View style={styles.horizontalFar}>
-                <Text style={styles.title}>{title}</Text>
-                <View style={{ flexDirection: 'row' }}>
-                    <Text style={styles.label}>Carbs: {Item.totalNutrients.CHOCDF.quantity.toFixed(3)} gm</Text>
-                    <Text style={styles.label}>Protein : {Item.totalNutrients.PROCNT.quantity.toFixed(3)} gm</Text>
-                </View>
-            </View>
-            <View style={styles.horizontalFar}>
-                <Text style={styles.label}>Total Calories : {Item.calories} Kcal</Text>
-                <Text style={styles.label}>Fats : {Item.totalNutrients.FAT.quantity.toFixed(3)} gm</Text>
-            </View>
-        </View>
-        </TouchableOpacity>
-    )
-}
-const styles = StyleSheet.create({
+const MealCard = ({Item, title, mealTime, refresh}) => {
+  const phone = auth().currentUser.phoneNumber;
+  const isDarkMode = useSelector((state) => state.isDarkMode);
+
+  const styles = StyleSheet.create({
     horizontalFar: {
-        justifyContent: 'space-between',
-        flexDirection: 'row',
+      justifyContent: 'space-between',
+      flexDirection: 'row',
     },
     container: {
-        borderTopWidth: .5,
-        borderBottomWidth: .5,
-        borderColor: Colors.primaryColorDark,
-        paddingVertical: 5,
-        paddingLeft: 15,
-        paddingRight: 5,
-        marginBottom: 2,
+      borderTopWidth: 0.5,
+      borderBottomWidth: 0.5,
+      borderColor: Colors.primaryColorDark,
+      paddingVertical: 5,
+      paddingLeft: 15,
+      paddingRight: 5,
+      marginBottom: 2,
     },
     label: {
-        fontSize: 12,
-        paddingRight: 10,
+      fontSize: 12,
+      paddingRight: 10,
+      color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80,
     },
     title: {
-        fontFamily: 'Karla-Bold',
-        fontSize: 15,
-        paddingBottom: 2,
-    }
-})
+      fontFamily: 'Karla-Bold',
+      fontSize: 15,
+      paddingBottom: 2,
+      color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80,
+    },
+  });
 
+  const deleteItem = async () => {
+    await database()
+      .ref('/Users/')
+      .child(phone)
+      .child('diet')
+      .child(mealTime)
+      .child(title)
+      .set(null)
+      .then(() => {
+        refresh();
+      });
+  };
+  return (
+    <TouchableOpacity
+      onLongPress={() => {
+        ConfirmDialog('Delete', 'Sure to Delete', deleteItem);
+      }}>
+      <View style={styles.container}>
+        <View style={styles.horizontalFar}>
+          <Text style={styles.title}>{title}</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={styles.label}>
+              Carbs: {Item.totalNutrients.CHOCDF.quantity.toFixed(3)} gm
+            </Text>
+            <Text style={styles.label}>
+              Protein : {Item.totalNutrients.PROCNT.quantity.toFixed(3)} gm
+            </Text>
+          </View>
+        </View>
+        <View style={styles.horizontalFar}>
+          <Text style={styles.label}>
+            Total Calories : {Item.calories} Kcal
+          </Text>
+          <Text style={styles.label}>
+            Fats : {Item.totalNutrients.FAT.quantity.toFixed(3)} gm
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 export default MealCard;
