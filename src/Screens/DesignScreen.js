4913a0eec5587d723c56c4398289e8f35715f32a
Govinda Vashishtha <govindavashishtha@gmail.com>
2021-07-04 23:26:05 +0530

    resolved design Screen add to diet error

--- src/Screens/DesignScreen.js
+++ src/Screens/DesignScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -21,8 +21,10 @@ import ThemeButtonDisabled from './../Components/ThemeButtonDisabled';
 import toast from './../Components/Toast';
 import database from '@react-native-firebase/database';
 import DietScreen from './DietScreen';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import DietIcon from './../assets/diet.svg';
+
+
 
 const DesignScreen = () => {
   const [query, setQuery] = useState('');
@@ -116,16 +118,16 @@ const DesignScreen = () => {
   });
 
   const apiKeys = [
-    {id: '4e18da1c', key: 'f3febcf06129e0a997f01ca869a8fc9b'},
-    {id: '637f0343', key: 'f11ca2c4da5076e81c83b2ba5aed2c56a'},
-    {id: 'e301e438', key: '10d90fe5af2e277e39179d43cbd2cd09'},
-    {id: '9d5826ac', key: '21d803b54cf439280a6a243119672e0d'},
-    {id: '53ec4d19', key: 'e61fdb3cd19724aa1bd74ce7db12b871'},
-    {id: '0dd5e843', key: '3ffcdd627a25d5dbf157a3a3854af603'},
+    { id: '4e18da1c', key: 'f3febcf06129e0a997f01ca869a8fc9b' },
+    { id: '637f0343', key: 'f11ca2c4da5076e81c83b2ba5aed2c56a' },
+    { id: 'e301e438', key: '10d90fe5af2e277e39179d43cbd2cd09' },
+    { id: '9d5826ac', key: '21d803b54cf439280a6a243119672e0d' },
+    { id: '53ec4d19', key: 'e61fdb3cd19724aa1bd74ce7db12b871' },
+    { id: '0dd5e843', key: '3ffcdd627a25d5dbf157a3a3854af603' },
   ];
 
   const tips = [
-    'Don’t eat sugar calories','Eat nuts','Avoid processed junk food (eat real food instead)','Don’t fear coffee','Eat fatty fish','Get enough sleep','Take care of your gut health with probiotics and fiber','Drink plenty of water, especially before meals','Don’t eat overcook or burnt meat','Avoid bright lights before sleep','Take vitamin D3 if you don’t get much sun exposure','Eat vegetables and fruits','Make sure to eat enough protein','Do some cardio','Don’t smoke or do drugs, and only drink in moderation','Use extra virgin olive oil','Minimize your sugar intake','Don’t eat a lot of refined carbs','Don’t fear saturated fat','Lift heavy','Avoid artificial trans fats','Use plenty of herbs and spices','Track your food intake every now and then','Exercise Every Day'
+    'Don’t eat sugar calories', 'Eat nuts', 'Avoid processed junk food (eat real food instead)', 'Don’t fear coffee', 'Eat fatty fish', 'Get enough sleep', 'Take care of your gut health with probiotics and fiber', 'Drink plenty of water, especially before meals', 'Don’t eat overcook or burnt meat', 'Avoid bright lights before sleep', 'Take vitamin D3 if you don’t get much sun exposure', 'Eat vegetables and fruits', 'Make sure to eat enough protein', 'Do some cardio', 'Don’t smoke or do drugs, and only drink in moderation', 'Use extra virgin olive oil', 'Minimize your sugar intake', 'Don’t eat a lot of refined carbs', 'Don’t fear saturated fat', 'Lift heavy', 'Avoid artificial trans fats', 'Use plenty of herbs and spices', 'Track your food intake every now and then', 'Exercise Every Day'
   ]
 
   const fecthTip = () => {
@@ -135,12 +137,13 @@ const DesignScreen = () => {
 
 
   const addToDiet = () => {
+    console.log(data);
     database()
       .ref('/Users')
       .child(phone)
       .child('diet')
       .child(mealTime)
-      .child(`${data.ingredients[0].text}`)
+      .child(query)
       .set(data)
       .then(() => {
         toast('Food added to your Diet.');
@@ -166,17 +169,17 @@ const DesignScreen = () => {
           <View style={styles.list}>
             <DropDownPicker
               items={[
-                {label: 'Breakfast', value: 'Breakfast'},
-                {label: 'Lunch', value: 'Lunch'},
-                {label: 'Pre-Workout', value: 'PreWorkout'},
-                {label: 'Post-Workout', value: 'PostWorkout'},
-                {label: 'Dinner', value: 'Dinner'},
+                { label: 'Breakfast', value: 'Breakfast' },
+                { label: 'Lunch', value: 'Lunch' },
+                { label: 'Pre-Workout', value: 'PreWorkout' },
+                { label: 'Post-Workout', value: 'PostWorkout' },
+                { label: 'Dinner', value: 'Dinner' },
               ]}
               placeholder="Select a meal time..."
-              containerStyle={{height: 40}}
+              containerStyle={{ height: 40 }}
               dropDownMaxHeight={100}
-              style={{backgroundColor: Colors.gray}}
-              dropDownStyle={{backgroundColor: Colors.gray}}
+              style={{ backgroundColor: Colors.gray }}
+              dropDownStyle={{ backgroundColor: Colors.gray }}
               onChangeItem={(item) => setMealTime(item.value)}
             />
             <View>
@@ -238,16 +241,19 @@ const DesignScreen = () => {
           <Header title={'Design'} />
           <View style={styles.iconcontainer}>
             <TouchableOpacity
+            style={{flexDirection:"row",justifyContent:'center',alignItems:'center', padding:4}}
               onPress={() => {
                 setIsDiet(false);
               }}>
               <Icon1
                 name={'food-apple'}
-                size={40}
+                size={20}
                 color={isDarkMode ? Colors.gray : Colors.charcoalGrey80}
               />
+              <Text style={{fontFamily: 'Karla-Bold',color : isDarkMode ? Colors.gray : Colors.charcoalGrey80, paddingLeft:5}}>My Diet</Text>
             </TouchableOpacity>
           </View>
+
           <ScrollView>
             <View style={styles.container}>
               <TouchableOpacity
@@ -282,30 +288,30 @@ const DesignScreen = () => {
               <Text style={styles.text}>
                 Search food item and get its Nutritional Values (e.g. 1 large apple)
               </Text>
-              { data ? (
+              {data ? (
                 <></>
               ) : (
-                <View style = {{justifyContent: 'center' ,alignItems: 'center', marginVertical: '50%'}}>
-                  <View style={{marginBottom: 20}}>
-                  <TouchableOpacity onPress={fecthTip}>
-                    <DietIcon />
-                  </TouchableOpacity>
+                <View style={{ justifyContent: 'center', alignItems: 'center', marginVertical: '50%' }}>
+                  <View style={{ marginBottom: 20 }}>
+                    <TouchableOpacity onPress={fecthTip}>
+                      <DietIcon />
+                    </TouchableOpacity>
                   </View>
-                  { tip && <Text style={{fontSize: 17,color:isDarkMode && Colors.white,fontFamily: 'Karla-Bold',padding: 10,textAlign: 'center',}}>{tip}</Text>}
-                <Text onPress={fecthTip} style={{fontSize: 13,color:isDarkMode && Colors.white,fontFamily: 'Karla-Regular',paddingVertical:0,textAlign: 'center',}}>Touch to get a pro tip </Text>
+                  {tip && <Text style={{ fontSize: 17, color: isDarkMode && Colors.white, fontFamily: 'Karla-Bold', padding: 10, textAlign: 'center', }}>{tip}</Text>}
+                  <Text onPress={fecthTip} style={{ fontSize: 13, color: isDarkMode && Colors.white, fontFamily: 'Karla-Regular', paddingVertical: 0, textAlign: 'center', }}>Touch to get a pro tip </Text>
                 </View>
-              ) }
+              )}
               {error ? (
                 <View style={styles.errorContainer}>
-                  <View style={{padding: 10}}>
+                  <View style={{ padding: 10 }}>
                     <Icon
                       name={'paper-plane'}
                       size={50}
                       color={isDarkMode ? Colors.cloudyWhite30 : Colors.charcoalGrey80}
                     />
                   </View>
-                  <Text style={{color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80}}>Sorry, Unable to search for this Query</Text>
-                  <Text style={{color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80}}>Try using another keywords</Text>
+                  <Text style={{ color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80 }}>Sorry, Unable to search for this Query</Text>
+                  <Text style={{ color: isDarkMode ? Colors.textColorDark : Colors.charcoalGrey80 }}>Try using another keywords</Text>
                 </View>
               ) : (
                 <View>
